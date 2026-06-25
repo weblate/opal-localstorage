906a2d60f59de34e77b9b643b2da3518b313cb03
Ladislav Hodas <ladislav@hodas.sk>
2026-06-25 19:01:29 +0200

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (9 of 9 strings)
    
    Co-authored-by: Ladislav Hodas <ladislav@hodas.sk>
    Translate-URL: https://hosted.weblate.org/projects/opal/opal-localstorage/sk/
    Translation: Opal for SailfishOS/Module: Opal.LocalStorage

--- translations/opal-localstorage-sk.ts
+++ translations/opal-localstorage-sk.ts
@@ -6,48 +6,48 @@
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="206"/>
         <source>Database upgrade failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktualizácia databázy zlyhala</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="207"/>
         <source>An error occurred while upgrading the database from version %1 to version %2. Please report this issue.</source>
-        <translation type="unfinished"></translation>
+        <translation>Pri aktualizácii databázy z verzie %1 na verziu %2 sa vyskytla chyba. Nahláste tento problém.</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="213"/>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="238"/>
         <source>Developer information:</source>
-        <translation type="unfinished"></translation>
+        <translation>Informácie pre vývojárov:</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="222"/>
         <source>Invalid database version</source>
-        <translation type="unfinished"></translation>
+        <translation>Neplatná verzia databázy</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="223"/>
         <source>The app cannot start because the database has version %1 but only version %2 is supported.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aplikácia sa nedá spustiť, pretože databáza má verziu %1, ale podporovaná je iba verzia %2.</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="230"/>
         <source>Database Maintenance</source>
-        <translation type="unfinished"></translation>
+        <translation>Údržba databázy</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="231"/>
         <source>Please be patient and allow up to 30 seconds for this.</source>
-        <translation type="unfinished"></translation>
+        <translation>Buďte trpezliví a počkajte aspoň 30 sekúnd.</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="234"/>
         <source>Database issue</source>
-        <translation type="unfinished"></translation>
+        <translation>Problém s databázou</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="235"/>
         <source>An unexpected issue occurred in the database. Try restarting the app.</source>
-        <translation type="unfinished"></translation>
+        <translation>V databáze sa vyskytol neočakávaný problém. Skúste reštartovať aplikáciu.</translation>
     </message>
 </context>
 </TS>
