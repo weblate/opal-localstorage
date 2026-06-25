1c0e76d01b78dccf94267589631fdcff40972f15
Automatic translation add-on <noreply-addon-autotranslate@weblate.org>
2026-06-25 19:01:30 +0200

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (9 of 9 strings)
    
    Co-authored-by: Automatic translation add-on <noreply-addon-autotranslate@weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/opal/opal-localstorage/ta/
    Translation: Opal for SailfishOS/Module: Opal.LocalStorage

--- translations/opal-localstorage-ta.ts
+++ translations/opal-localstorage-ta.ts
@@ -6,33 +6,33 @@
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="206"/>
         <source>Database upgrade failed</source>
-        <translation type="unfinished"></translation>
+        <translation>தரவுத்தள மேம்படுத்தல் தோல்வியடைந்தது</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="207"/>
         <source>An error occurred while upgrading the database from version %1 to version %2. Please report this issue.</source>
-        <translation type="unfinished"></translation>
+        <translation>தரவுத்தளத்தை பதிப்பு %1 இலிருந்து பதிப்பு%2க்கு மேம்படுத்தும் போது பிழை ஏற்பட்டது. இந்தச் சிக்கலைப் புகாரளிக்கவும்.</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="213"/>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="238"/>
         <source>Developer information:</source>
-        <translation type="unfinished"></translation>
+        <translation>உருவாக்குபவர் தகவல்:</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="222"/>
         <source>Invalid database version</source>
-        <translation type="unfinished"></translation>
+        <translation>தவறான தரவுத்தள பதிப்பு</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="223"/>
         <source>The app cannot start because the database has version %1 but only version %2 is supported.</source>
-        <translation type="unfinished"></translation>
+        <translation>தரவுத்தளமானது பதிப்பு %1 ஐக் கொண்டிருப்பதால் பயன்பாட்டைத் தொடங்க முடியாது, ஆனால் பதிப்பு %2 மட்டுமே ஆதரிக்கப்படுகிறது.</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="230"/>
         <source>Database Maintenance</source>
-        <translation type="unfinished">தரவுத்தள பராமரிப்பு</translation>
+        <translation>தரவுத்தள பராமரிப்பு</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="231"/>
@@ -42,12 +42,12 @@
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="234"/>
         <source>Database issue</source>
-        <translation type="unfinished"></translation>
+        <translation>தரவுத்தள சிக்கல்</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="235"/>
         <source>An unexpected issue occurred in the database. Try restarting the app.</source>
-        <translation type="unfinished"></translation>
+        <translation>தரவுத்தளத்தில் எதிர்பாராத சிக்கல் ஏற்பட்டது. பயன்பாட்டை மறுதொடக்கம் செய்ய முயற்சிக்கவும்.</translation>
     </message>
 </context>
 </TS>
