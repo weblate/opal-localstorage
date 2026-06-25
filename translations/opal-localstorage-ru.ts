77d90644e2bd24687c8a93188256b55a15a01984
ShowhyT <showhy@proton.me>
2026-06-25 19:01:26 +0200

    Translated using Weblate (Russian)
    Currently translated at 100.0% (9 of 9 strings)
    
    Co-authored-by: ShowhyT <showhy@proton.me>
    Translate-URL: https://hosted.weblate.org/projects/opal/opal-localstorage/ru/
    Translation: Opal for SailfishOS/Module: Opal.LocalStorage

--- translations/opal-localstorage-ru.ts
+++ translations/opal-localstorage-ru.ts
@@ -6,33 +6,33 @@
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="206"/>
         <source>Database upgrade failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Не удалось обновить базу данных</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="207"/>
         <source>An error occurred while upgrading the database from version %1 to version %2. Please report this issue.</source>
-        <translation type="unfinished"></translation>
+        <translation>Произошла ошибка при обновлении базы данных с %1 до версии %2. Пожалуйста, сообщите об этой проблеме.</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="213"/>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="238"/>
         <source>Developer information:</source>
-        <translation type="unfinished"></translation>
+        <translation>Информация о разработчике:</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="222"/>
         <source>Invalid database version</source>
-        <translation type="unfinished"></translation>
+        <translation>Неверная версия базы данных</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="223"/>
         <source>The app cannot start because the database has version %1 but only version %2 is supported.</source>
-        <translation type="unfinished"></translation>
+        <translation>Приложение не может быть запущено, поскольку база данных имеет версию %1, но поддерживается только версия %2.</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="230"/>
         <source>Database Maintenance</source>
-        <translation type="unfinished"></translation>
+        <translation>Обслуживание Базы Данных</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="231"/>
@@ -42,12 +42,12 @@
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="234"/>
         <source>Database issue</source>
-        <translation type="unfinished"></translation>
+        <translation>Проблема с базой данных</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="235"/>
         <source>An unexpected issue occurred in the database. Try restarting the app.</source>
-        <translation type="unfinished"></translation>
+        <translation>Произошла непредвиденная ошибка с базой данных. Попробуйте перезапустить приложение.</translation>
     </message>
 </context>
 </TS>
